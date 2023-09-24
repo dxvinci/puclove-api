b4e4eae09e54a7bd30ca991a3cc1c57c9bd11927
Davi Ribeiro <davirpa1@gmail.com>
2023-09-24 17:01:56 -0300

    feat: added findInterestById method

--- src/main/java/com/example/puclove/interest/InterestRepository.java
+++ src/main/java/com/example/puclove/interest/InterestRepository.java
@@ -4,8 +4,11 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InterestRepository extends MongoRepository<Interest, ObjectId> {
 
+    Optional<Interest> findInterestById(ObjectId id);
 
 }
