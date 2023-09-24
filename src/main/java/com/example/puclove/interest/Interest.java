aba287a4d6358326aecfc6150ba692883b973d1a
Davi Ribeiro <davirpa1@gmail.com>
2023-09-24 14:41:03 -0300

    add required annotations

--- src/main/java/com/example/puclove/interest/Interest.java
+++ src/main/java/com/example/puclove/interest/Interest.java
@@ -1,10 +1,18 @@
 package com.example.puclove.interest;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "interests")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Interest {
 
     @Id
