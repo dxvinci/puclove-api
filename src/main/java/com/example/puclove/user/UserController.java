1ffdf6f42b4e8e87381c8dd32ff2256bb0d223d2
Davi Ribeiro <davirpa1@gmail.com>
2023-09-28 22:10:27 -0300

    added @CrossOrigin annotation to fix CORS error

--- src/main/java/com/example/puclove/user/UserController.java
+++ src/main/java/com/example/puclove/user/UserController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController()
+@CrossOrigin("http://localhost:5173")
 @RequestMapping("/api/v1/users")
 public class UserController {
     @Autowired
